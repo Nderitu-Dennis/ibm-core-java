d511f5f1ded548b9f55c0ba45d529ecfe7a203ae
Nderitu-Dennis <cit2270322021@mmu.ac.ke>
2025-02-05 17:44:53 +0300

    learnt about ArrayList and how dynamic it is. it allocates spaces even without specifying the capacity. size means the current elements in the ArrayList and the capacity is total no. of elements it can hold before resizing.

--- src/wrapper_classes/Main.java
+++ src/wrapper_classes/Main.java
@@ -1,4 +1,7 @@
 package wrapper_classes;
+
+import java.util.ArrayList;
+
 /*
 * wrapper classes provide a way to treat primitive data as objects which is necessary
 * for certain Java features such as using them with collections such as ArrayList and Hashmaps
@@ -8,6 +11,21 @@ public class Main {
         String number = "123";
         int x = Integer.parseInt(number);
         System.out.println(x+20);
+
+        ArrayList<String> studentNames = new ArrayList<>();
+        studentNames.add("dennis nderitu");
+        studentNames.add("mazindere oluoch");
+        studentNames.add("otorongo jogothe");
+        studentNames.add("ng'oe wairimu");
+
+        System.out.println(studentNames);
+
+      //  studentNames.remove(1);
+        System.out.println(studentNames);
+        System.out.println("size of the ArrayList: " + studentNames.size());
+        System.out.println("at index 0: " + studentNames.get(0));
+
+
 
     }
 
