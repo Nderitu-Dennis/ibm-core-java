40132ef02e59975b43d104c2ac0c2b0d5e0bafa9
Nderitu-Dennis <cit2270322021@mmu.ac.ke>
2025-02-10 13:42:55 +0300

    learnt abt linked lists and how they can be used to either implement a queue or a stack.also abt consuming new line esp after using nextInt() cz they read only a phrase not the entire line like nextLine()

---
+++ src/linked_list/LinkedListPractise.java
@@ -0,0 +1,68 @@
+
+package linked_list;
+
+//using a (to_do) to learn about linked lists
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class LinkedListPractise {
+    //add a task
+    //remove
+    //view
+
+    public static void main(String[] args) {
+        LinkedList<String> tasks = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.println("1.Add a task\t2.View all tasks\t3.Remove a task\t4.Close the app\n\tEnter your choice:");
+            int choice=scanner.nextInt(); //user enters a choice
+           scanner.nextLine(); // Consume the leftover newline
+
+            /*
+            * You need to consume the newline only because scanner.nextInt()
+            * (and similar methods like nextDouble(), next() etc.) do not consume the newline
+            * (\n) after reading a number or word.*/
+
+            switch (choice){
+                case 1:
+                    System.out.print("Enter a task: ");
+                    //String variable for storing task entered by user
+                    String task = scanner.nextLine();
+                    tasks.addLast(task);
+                    break;
+
+                case 2:
+                    if(!tasks.isEmpty()){
+                        System.out.println("displaying all tasks:");
+                        for(String t : tasks){
+                            System.out.println(t);
+                        }
+                        System.out.println();
+
+                    }else{
+                        System.out.println("no task to display");
+                    }
+                    break;
+
+                case 3:
+                    if(!tasks.isEmpty()){
+                    System.out.println("removed " + tasks.removeFirst());}
+                    else{
+                        System.out.println("no task to remove");
+
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("closing...");
+                    System.exit(0);
+
+            }
+        }
+
+    }
+
+}
+
---
+++ src/linked_list/SwitchCasePractise.java
@@ -0,0 +1,44 @@
+package linked_list;
+
+import java.util.Scanner;
+
+public class SwitchCasePractise {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            int code;
+            System.out.println("\n**courses at MMU**\n1.227\t2.223\t3.225\t4.221\tpress 0 to exit\nEnter the code to see your course:");
+            //capturing input from user and validating
+
+            if(!sc.hasNextInt()){
+                System.out.println("error! please enter an integer");
+                sc.next(); //clear the scanner
+            }
+            else {
+            code=sc.nextInt();
+
+            if(code==0)
+                System.exit(0);
+
+            switch (code){
+                case 227:
+                    System.out.println("SE");
+                    break;
+                case 223:
+                    System.out.println("CS");
+                    break;
+                case 225:
+                    System.out.println("CT");
+                    break;
+                case 221:
+                    System.out.println("IT");
+                    break;
+                default:
+                    System.out.println("error");
+
+            }
+
+        }
+    }}
+}
