7ae9f06cd9d8065a628fac902dd28cb07ffc4464
Nderitu-Dennis <nderitudenis87@gmail.com>
2025-11-07 10:54:02 +0300

    add hashmaps/Student.java

--- src/collections/arraylists/Book.java
+++ src/collections/arraylists/Book.java
@@ -47,19 +47,19 @@ public class Book {
         this.isBorrowed = isBorrowed;
     }
 
-  public String toString(){
+    public String toString() {
         return id + ", " + title + ", " + author + ", " + isBorrowed;
 
-   }
+    }
 
     public static void main(String[] args) {
         ArrayList<Book> book = new ArrayList<>();
 
-        book.add(new Book(1,"Bible","Dennis the Black Jesus",true));
-        book.add(new Book(2,"Dictionary","Macmillan",false));
-        book.add(new Book(3,"Poems","Mazindere",false));
-        book.add(new Book(4,"Things Fall Apart","Chinua Achebe",true));
-        book.add(new Book(5,"Gods must be crazy","Huang",false));
+        book.add(new Book(1, "Bible", "Dennis the Black Jesus", true));
+        book.add(new Book(2, "Dictionary", "Macmillan", false));
+        book.add(new Book(3, "Poems", "Mazindere", false));
+        book.add(new Book(4, "Things Fall Apart", "Chinua Achebe", true));
+        book.add(new Book(5, "Gods must be crazy", "Huang", false));
 
         System.out.println("\nsize: " + book.size());
         System.out.println("Book details at index 0: " + book.get(0));
@@ -67,25 +67,21 @@ public class Book {
         System.out.println("\n" + book + "\n"); // prints all records as one array
 
 
-        for(Book b : book){
+        for (Book b : book) {
             System.out.println(b.toString());
 
         }
 
         book.remove(4);
-        System.out.println( "\nremoved element 5 \n");
+        System.out.println("\nremoved element 5 \n");
 
-        for(Book b : book){
+        for (Book b : book) {
             System.out.println(b.toString());
 
         }
 
 
         System.out.println("size: " + book.size());
-
-
-
-
 
 
     }
---
+++ src/collections/hashmaps/Student.java
@@ -0,0 +1,77 @@
+package collections.hashmaps;
+/* HashMaps- same as dictionary in python. Stores data in key-value pairs
+ and keys must be unique, no duplicate keys. used whenever fast lookup is needed
+   cz u just search using the key. */
+
+import java.util.HashMap;
+
+public class Student {
+    public static void main(String[] args) {
+        HashMap<String, Integer> studentMarks = new HashMap<>();
+
+        studentMarks.put("Dennis the legend", 40);
+        studentMarks.put("Allan Njogothe", 92);
+        studentMarks.put("Kelvin the lover boy", 76);
+        studentMarks.put("Sunil mohammed", 48);
+        studentMarks.put("Greyson marcus", 10);
+
+//        printing the marks
+        System.out.println("\n" + studentMarks);
+
+        //  getting the size
+        System.out.println("no. of students/size: " + studentMarks.size());
+
+
+        System.out.println("\n**printing keys using enhanced for loop**\n");
+
+        for (String keys : studentMarks.keySet()) {
+            System.out.println(keys);
+        }
+
+        System.out.println("\n**printing values using enhanced for loop**\n");
+
+        for (Integer values : studentMarks.values()) {
+            System.out.println(values);
+        }
+
+//        remove
+        studentMarks.remove("Sunil mohammed");
+        System.out.println("after removing key sunil..\n" + studentMarks);
+        System.out.println("no. of students after the above remove(): " + studentMarks.size());
+
+//        Given a student’s name, print their mark.
+        System.out.println("\nmarks for Greyson: " + studentMarks.get("Greyson marcus"));
+
+       //adding an element
+        studentMarks.put("Nicodemus", 90);
+
+//        Find and print the student with the highest mark.
+        System.out.println("\nFind and print the student with the highest mark");
+        int max = 0;
+        for (String studentMark : studentMarks.keySet()) {
+            System.out.println(studentMark + ": " + studentMarks.get(studentMark));
+            if (studentMarks.get(studentMark) > max) {
+                max = studentMarks.get(studentMark);
+            }
+
+        }
+        System.out.println("HIGHEST MARK: " + max);
+
+//        Count how many students scored above 50.
+        System.out.println("\nCount how many students scored above 50.");
+        int counter = 0;
+        for (String studentName : studentMarks.keySet()) {
+            if (studentMarks.get(studentName) > 50) {
+                counter++;
+                System.out.println(studentName + ": " + studentMarks.get(studentName));
+            }
+
+        }
+
+        System.out.println("students with marks greater than 50: " + counter);
+
+
+    }
+
+
+}
